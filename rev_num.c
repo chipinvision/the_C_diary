ae7e80a30ee46eff165e43a56aa46b933982e757
Suresh Mishra <72114434+chipinvision@users.noreply.github.com>
2022-10-04 19:08:41 +0530

    #13

---
+++ rev_num.c
@@ -0,0 +1,12 @@
+// PRINTING ALL NATURAL NUMBERS IN REVERSE
+#include<stdio.h>
+
+int main(){
+    int num = 10;
+
+    for(int i = num; i>=1; --i){
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
