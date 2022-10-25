ccf6cda713044805e8ed001fb69036c23bbf72bb
Suresh Mishra <72114434+chipinvision@users.noreply.github.com>
2022-10-25 19:47:29 +0530

    #21

---
+++ fibonacci.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+
+int main(){
+    
+    int n1=0, n2=1, n3, i, num;
+
+    printf("Number of Elements: ");
+    scanf("%d", &num);
+    printf("\n%d%d",n1, n2);
+    for(i=2;i<num;++i){
+        n3=n1+n2;
+        printf("%d",n3);
+        n1=n2;
+        n2=n3;
+    }
+
+    return 0;
+}
