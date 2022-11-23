5696c9e6c272fe6479f7f30140b9b1ab7185a7c0
Suresh Mishra <72114434+chipinvision@users.noreply.github.com>
2022-11-23 15:04:42 +0530

    #29

---
+++ area_square.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+
+int main(){
+	
+	int side, area;
+	
+	printf("Enter the side of square: ");
+	scanf("%d", &side);
+	
+	area = side*side;
+	
+	printf("Area of Square: %d", area);
+	
+	return 0;
+	
+}
